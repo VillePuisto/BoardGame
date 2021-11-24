9e06fb101f2c584e029a4f34cb733d7e325b3ef8
Ville Puisto <60825081+VillePuisto@users.noreply.github.com>
2021-11-24 22:03:07 +0200

    lisätty all board games sivu

--- boardgames/urls.py
+++ boardgames/urls.py
@@ -5,4 +5,5 @@ app_name= 'boardgames'
 urlpatterns= [
     #Home page
     path('', views.index, name='index'),
+    path('games/', views.games, name='games'),
 ]
--- boardgames/views.py
+++ boardgames/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Game
 
 # Create your views here.
 
 def index(request):
     """The home page for Board Game Website"""
     return render(request, 'boardgames/index.html')
+
+def games(request):
+    """The page for All Board Games"""
+    games = Game.objects.order_by('date_added')
+    context = {'games': games}
+    return render(request, 'boardgames/games.html', context)
