f48eaf62dd59c3b015ac85f9074ff1c2d3bfc7a2
Sarianna Junnila <sarianna.junnila@edu.turkuamk.fi>
2021-11-26 15:07:53 +0200

    Yksittäisen pelin sivu ja lainaus
    GameInfo -> nyt Loan

--- boardgames/urls.py
+++ boardgames/urls.py
@@ -8,4 +8,8 @@ urlpatterns= [
     path('games/', views.games, name='games'),
     #Page for adding a new game
     path('new_game/', views.new_game, name='new_game'),
+    #Page for a single game
+    path('games/<int:game_id>/', views.game, name='game'),
+    #Page for loaning a game
+    path('new_loan/<int:game_id>/', views.new_loan, name='new_loan'),
 ]
