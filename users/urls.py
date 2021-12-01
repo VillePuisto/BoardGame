8549794817170aee65771a734e05b987ae74d0fb
Ville Puisto <60825081+VillePuisto@users.noreply.github.com>
2021-12-01 18:30:06 +0200

    korjattu_registration

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name= 'users'
 urlpatterns= [
     #Include default auth urls.
-path('', include('django.contrib.auth.urls')), 
-path('register/', views.register, name='register'),
+    path('', include('django.contrib.auth.urls')), 
+    path('register/', views.register, name='register'),
 ]
